3436bfeb7a072315f423a54f7875f6a829e16439
Julian Lam <julian@nodebb.org>
2020-05-12 14:36:17 -0400

    fix: only affect opacity of post-header and content, not the entire post block

--- assets/dwnvtr.js
+++ assets/dwnvtr.js
@@ -4,11 +4,11 @@
 $(document).ready(function() {
 	$(window).on('action:topic.loaded', function() {
 		// Reduce post by 5% opacity for each downvote received
-		var	posts = $('.posts li[data-pid]'),
+		var	posts = $('.posts li[data-pid] .post-header, .posts li[data-pid] .content'),
 			rep, opacity;
 
 		posts.each(function(idx, postEl) {
-			rep = parseInt($(postEl).find('[data-votes]').attr('data-votes'), 10);
+			rep = parseInt($(postEl).parents('li').find('[data-votes]').attr('data-votes'), 10);
 
 			if (rep < 0) {
 				opacity = 1 + (rep*0.20);
